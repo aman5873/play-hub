c8d1d194f7ff8032316eff65d8a845b4852aa33c
Aman chauhan <chauhanaman4002@gmail.com>
2025-09-03 19:38:06 +0530

    change in config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    turbo: false,
+    optimizeCss: false, // 👈 forces PostCSS instead of LightningCSS
+  },
   images: {
     domains: ["images.unsplash.com"], // allow Unsplash images
   },
